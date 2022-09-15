d1e6c1a912e40a1eef6ac4ca4d6dcbf948390ed2
hrome <58020216+hromero7@users.noreply.github.com>
2022-09-15 14:12:08 -0700

    fixing api call on frontend

--- client/src/utils/NewsAPI.js
+++ client/src/utils/NewsAPI.js
@@ -3,12 +3,12 @@ import axios from "axios";
 
 export default {
     getTrending: () => {
-        return axios.get(`http://localhost:3001/news/trending`);
+        return axios.get(`/news/trending`);
     },
     getCategoryNews: (category) => {
-        return axios.get(`http://localhost:3001/news/category/${category}`);
+        return axios.get(`/news/category/${category}`);
     },
     getSearchQuery: (query) => {
-        return axios.get(`http://localhost:3001/news/search/${query}`);
+        return axios.get(`/news/search/${query}`);
     }
 }
